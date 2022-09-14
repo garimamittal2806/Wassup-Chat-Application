a028898ca09f71428e6131a309ead943ac9c5ec1
garimamittal2806 <72308836+garimamittal2806@users.noreply.github.com>
2022-09-14 22:31:23 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,50 @@
+const socket = io()
+
+let name
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.msg_area')
+do {
+    name = prompt('please enter your name')
+} while (!name)
+
+textarea.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    //append
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+    //send to server
+    socket.emit('message', msg)
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markUp = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markUp
+
+    messageArea.appendChild(mainDiv)
+}
+//recieve msg
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
